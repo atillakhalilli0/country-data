6a96e378cd86d0d5e95695d3afb12974f0cf419e
atillakhalilli0 <atilla.xalilli@gmail.com>
2025-03-31 17:19:53 +0400

    com9

--- navbar.js
+++ navbar.js
@@ -20,7 +20,7 @@ function showRegions() {
       
       const desktopMenu = document.getElementById("desktop-regions-menu")
       desktopMenu.innerHTML = uniqueRegions.map(region => `
-        <a href="https://country-explorer-by-atilla.vercel.app/regions.html?id=${region}" class="px-3 py-2 text-gray-700 hover:text-indigo-600 hover:underline transition duration-300">${region}</a>
+        <a href="https://country-explorer-by-atilla.vercel.app/regions.html?id=${region}" class="px-3 py-2 text-yellow-500 hover:text-indigo-300 hover:underline transition duration-300">${region}</a>
       `).join("")
       
       mobileRegionsMenu.innerHTML = uniqueRegions.map(region => `
